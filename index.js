1e934ec6a9b18dc1f3d3987dfb263c17d5139b44
heesane <tees3359@gmail.com>
2023-04-25 01:40:42 +0900

    Bug Fix

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const swaggerDocument = require('./swagger.json');
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get('/',(req,res)=>{
-  res.send("Hello");
-});
+app.use(cors());
 
 // API 엔드포인트 등록
 app.get('/api/users', (req, res) => {
   // 사용자 목록 반환
+  res.json({
+    "Hello":"World!"
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, () => {
+  console.log('Example app listening on port 3001!');
 });
